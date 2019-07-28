47bea9dd745556685b76a7864291aac0914fd09f
MD-AZMAL <azmalmd12@gmail.com>
2019-07-28 22:07:42 +0530

    escape!!

--- escapeit.py
+++ escapeit.py
@@ -1,13 +1,19 @@
 import speech_recognition as sr  
+from pynput.keyboard import Key, Controller
 
 r = sr.Recognizer()
+Keyboard = Controller()
 
 while True:              
     with sr.Microphone() as source: 
-        print("Say escape : ",end='')
+        print("Say escape : ")
         audio = r.listen(source)     
         try:
             text = r.recognize_google(audio)    
             print("{}".format(text))
+            if 'escape' in text:
+                Keyboard.press(Key.esc)
+                Keyboard.release(Key.esc)
+
         except:
             print("Sorry could not recognize your voice")   
